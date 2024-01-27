b622359c7ecc4988850a2148a10d18d633d0b4b0
Mykyta Medvediev <mednikita2004@gmail.com>
2024-01-27 17:22:57 +0200

    so, it works

--- force-app/main/default/lwc/headerConfiguration/headerConfiguration.js
+++ force-app/main/default/lwc/headerConfiguration/headerConfiguration.js
@@ -4,9 +4,11 @@ import USER_NAME from '@salesforce/schema/User.Name';
 import USER_ID from '@salesforce/user/Id';
 //apex methods
 import getUsers from '@salesforce/apex/UserHandler.getUsers'; //returns all users in the org
-import getObjectivitiesOptionsAccordingUser from '@salesforce/apex/ObjectivesHandler.getObjectivitiesOptionsAccordingUser';
-import getKeyRecords from '@salesforce/apex/KeyResultHandler.getKeyRecords';
+import getKeyRecordsOptions from '@salesforce/apex/KeyResultHandler.getKeyRecordsOptions';
 import createReview from '@salesforce/apex/KeyResultHandler.createReview'; // creates a record for Review__c object
+import createGoogleReview from '@salesforce/apex/KeyResultHandler.createGoogleReview';
+import createSurvey from '@salesforce/apex/KeyResultHandler.createSurvey';
+import createCaseStudy from '@salesforce/apex/KeyResultHandler.createCaseStudy';
 
 export default class HeaderConfiguration extends LightningElement 
 {
@@ -18,6 +20,14 @@ export default class HeaderConfiguration extends LightningElement
     @track yearOptions= []; //options for combobx to display assigned objectives with corresponig year
     @track activityButtons = []; //options for combobx to display buttons that can create objective, keyresult and so on.
 
+    @track action;
+    @track keyResultsRecords; //upload all keyResults records to assign them to related fields
+    @track formData = []; // array that contain parameters to fetch to apex method to create a related field
+    //show templates modal
+    @track isCreateReview = false;
+    @track isCreateGoogleReview = false;
+    @track isCreateSurvey = false;
+    @track isCreateCaseStudy = false;
     //automatically get user id that is chosen from combobox
     @wire(getRecord,{recordId: '$userId',fields:[USER_NAME]})
     wiredUser({ error, data }) {
@@ -30,24 +40,18 @@ export default class HeaderConfiguration extends LightningElement
         this.loadUserOptions(); 
         this.getNextTenYears();
         this.loadButtons();
-        //this.loadKeyRecords();
+        this.loadKeyRecords();
     }
-    // async loadKeyRecords(){
-    //     try{
-    //         this.keyResultsRecords = await getKeyRecords();
-    //         //let keyResults = await getKeyRecords();
-    //         // console.log(keyResults)
-    //         // for(let i = 0; i < keyResults.length ;i++)
-    //         //     {
-    //         //         this.keyResultsRecords.push({label:`${keyResults[i]}`,value:`${keyResults[i]}`})
-    //         //     }
-    //         console.log('leyresultRecords', JSON.stringify(this.keyResultsRecords))
-    //     }
-    //     catch(error){
-    //         console.log('Error in loading keyResults', error)
-    //     }
-    // }
-
+    //load keyResult records
+    async loadKeyRecords(){
+        try{
+            let result = await getKeyRecordsOptions();
+            this.keyResultsRecords = result;
+        }
+        catch(error){
+            console.log('Error in loading keyResults', error)
+        }
+    }
     //retrieve ten next years in the org to options for combobox
     getNextTenYears(){
         let currentYear = new Date().getFullYear()-2;
@@ -61,57 +65,115 @@ export default class HeaderConfiguration extends LightningElement
     }
     //retrive all activity buttons
     loadButtons(){
-        let buttons = ['Add objectivity', 'Add key result', 'New review', 'New survey', 'New case study', 'New google review']
+        let buttons = ['New review', 'New survey', 'New case study', 'New google review']
         for(let i = 0; i < buttons.length ;i++)
         {
             this.activityButtons.push({label:`${buttons[i]}`,value:`${buttons[i]}`})
         }
     }
-    //@track isCreateReview = false;
-    //@track formData = [];
-
     //Handlers for buttons
     handleOpeningCreatingReview(){
         this.isCreateReview = false;
     }
-    // async handleSubmitReview(){
-    //     for(let prop in this.refs){
-    //         this.formData[prop] = this.refs[prop].value
-    //     }
-    //     console.log('formdata',JSON.stringify(this.formData['keyResult']))
-    //     console.log('formdata ID',this.formData['keyResult'])
-    //     console.log('formdata NAME',this.formData['reviewName'])
-
-    //     try {
-    //         aaa = await createReview({
-    //             reviewName: this.formData['reviewName'],
-    //             keyResultId: this.formData['keyResult']
-    //         })
-    //     } catch (error) {
-    //         console.log('Error in creating a Review',JSON.stringify(error))
-    //         //console.log(this.a)
-    //     }
-    // }
+    handleOpeningCreatingGoogleReview(){
+        this.isCreateGoogleReview = false;
+    }
+    handleOpeningCreatingSurvey(){
+        this.isCreateSurvey = false;
+    }
+    handleOpeningCreatingCaseStudy(){
+        this.isCreateCaseStudy = false;
+    }
     // handle a choice from checkbox of buttons to provide some activity
     provideActivity(event){
         if(event.detail.value == 'New review'){
             this.isCreateReview = true;
         }
-        this.action = event.detail.value;
-        console.log('action', JSON.stringify(this.action ))
+        if(event.detail.value == 'New google review'){
+            this.isCreateGoogleReview = true;
+        }
+        if(event.detail.value == 'New survey'){
+            this.isCreateSurvey = true;
+        }
+        if(event.detail.value == 'New case study'){
+            this.isCreateCaseStudy = true; 
+        }
+        this.action = event.detail.value;      
     }
     //handle the changes in user combobox
     switchUserObjectivities(event){
-        this.userId=event.detail.value;
+        this.userId = event.detail.value;
     }
     //handle changes in year combobox
     switchYearObjectivities(event){
         this.objectivityYear = event.detail.value;
     }
-    // @track objList = [];
-    // async getObj(){
-    //     this.objList = await getObjectivitiesOptionsAccordingUser()
-    // }
+    //handlers ro submit data to apex method to reate related objects record for Key_Result__c
+    async handleSubmitReview(){
+        this.formData = [];
+        for(let prop in this.refs){
+            this.formData[prop] = this.refs[prop].value
+        }
+        try {
+            await createReview({
+                reviewName: this.formData['reviewName'],
+                keyResultId: this.formData['keyResult']
+            })
+            console.log('Review is created!')
+        } catch (error) {
+            console.log('Error in creating a Review',JSON.stringify(error))
+        }
+        this.isCreateReview = false;
+     }
+     async handleSubmitGoogleReview(){
+        this.formData = [];
+        for(let prop in this.refs){
+            this.formData[prop] = this.refs[prop].value
+        }
+        try {
+            await createGoogleReview({
+                googleReviewName: this.formData['googleReviewName'],
+                keyResultId: this.formData['keyResult']
+            })
+            console.log('Google Review is created!')
+        } catch (error) {
+            console.log('Error in creating a Google Review',JSON.stringify(error))
+        }
+        this.isCreateGoogleReview = false;
+     }
+     async handleSubmitSurvey(){
+        this.formData = [];
+        for(let prop in this.refs){
+            this.formData[prop] = this.refs[prop].value
+        }
+        try {
+            await createSurvey({
+                surveyName: this.formData['surveyName'],
+                keyResultId: this.formData['keyResult']
+            })
+            console.log('Survey is created!')
+        } catch (error) {
+            console.log('Error in creating a Survey',JSON.stringify(error))
+        }
+        this.isCreateSurvey = false;
+     }
+     async handleSubmitCaseStudy(){
+        this.formData = [];
+        for(let prop in this.refs){
+            this.formData[prop] = this.refs[prop].value
+        }
+        try {
+            await createCaseStudy({
+                caseStudyName: this.formData['caseStudyName'],
+                keyResultId: this.formData['keyResult']
+            })
+            console.log('Case Study is created!')
+        } catch (error) {
+            console.log('Error in creating a Survey',JSON.stringify(error))
+        }
+        this.isCreateSurvey = false;
+     }
+
     @track trackedFields = []
     getTrackFields(event){
         this.trackedFields = event.detail;
--- force-app/main/default/lwc/keyResultList/keyResultList.js
+++ force-app/main/default/lwc/keyResultList/keyResultList.js
@@ -1,7 +1,9 @@
-import { LightningElement,api,track } from 'lwc';
+import { LightningElement,api,track,wire } from 'lwc';
 //apex methods
 import getKeyResults from '@salesforce/apex/KeyResultHandler.getKeyResults';
-const RELATED_FIELD_OPTION = ['Opportunity', 'Google Review', 'Review', 'Case Study', 'Act', 'Survey', 'Call','Event', 'Leads (Web)']
+import updateKeyResultProgress from '@salesforce/apex/KeyResultHandler.updateKeyResultProgress'
+import getContractTypes from '@salesforce/apex/ContractsHandler.getContractTypes';
+const RELATED_FIELD_OPTION = ['Opportunity', 'Google Review', 'Review', 'Case Study', 'Act', 'Survey', 'Call','Event', 'Leads (Web)','Contract']
 export default class KeyResultList extends LightningElement 
 {
     @api keyResultId; //parameter from objectivityList component to retrieve all key results
@@ -10,20 +12,87 @@ export default class KeyResultList extends LightningElement
     @track showTargetWindow=false; // open SetTarget Window
     @track relatedFieldsOptions; // array store all related fields 
     @track targets; // contain key-value pair: RelatedObj-amout of these objects conneted with one keyResult
+    //пробую собрать значения с полей 
+    @track opportunityTarget = -1;
+    @track reviewTarget = -1;
+    @track googleReviewTarget = -1;
+    @track surveyTarget = -1;
+    @track caseStudyTarget = -1;
+    @track callTarget = -1;
+    @track actTarget = -1;
+    @track leadTarget = -1;
+    @track eventTarget = -1;
+    @track contractTarget = -1
+    @track savedTargets; // download from localStorage 
+    
+    @track showPicklist = false;
+    @track contractType;
+    //коллекция которая хранит в себе ключ это имя опции выбраная в SetTargetWinodow и значение которое будет изменятся при вводе input
+    @track dictionary = {
+        'Opportunity': this.opportunityTarget,
+        'Review': this.reviewTarget,
+        'Google Review': this.googleReviewTarget,
+        'Act': this.actTarget,
+        'Leads (Web)': this.leadTarget,
+        'Case Study': this.caseStudyTarget,
+        'Call': this.callTarget,
+        'Event': this.eventTarget,
+        'Survey': this.surveyTarget,
+        'Contract': this.contractTarget
+    }
     //autolaunched methods
     connectedCallback(){
-       this.loadKeyresults();
+       let result = JSON.parse(localStorage.getItem(this.keyResultId));
+       this.loadKeyresults(result);
+       this.loadPicklist();
     }
     //get keyResult by its id and get all options for checkboxgroup
-    async loadKeyresults(){
+    async loadKeyresults(result){
         try{
-        this.keyResults = await getKeyResults({keyResultId: this.keyResultId}); //responible for getting all keyResult records
+        console.log('result', JSON.stringify(result))
+        if(result && result.length > 0){
+            if('contractType' in result[0]){
+                console.log('CONTRACT TYPE FOUND:',result[0].contractType); // точно не пустой
+                this.keyResults = await getKeyResults({keyResultId: this.keyResultId, contractType : result[0].contractType});
+            }
+            else {
+                this.keyResults = await getKeyResults({keyResultId: this.keyResultId, contractType: null});
+            }
+        }
+        else{
+            this.keyResults = await getKeyResults({keyResultId: this.keyResultId, contractType: null});
+        }
         this.relatedFieldsOptions = RELATED_FIELD_OPTION.map(field=>({ label: field, value: field }));
         }
         catch(error){
-            console.log('load keyResults error: ',JSON.stringify(error));
+            console.log('load keyResults error: ',error);
+        }
+        let sumOfTargets = 0;
+        let listOfTargets = [];
+        let sumofCompletedTargets = 0;
+        if(result!==null){
+            this.savedTargets = await Promise.all (result.map(async item => {
+                let value = await this.getAmountOfRelatedField(item.label);
+                sumOfTargets = sumOfTargets + parseInt(item.setTarget);
+                listOfTargets.push(item.label.toString());
+                sumofCompletedTargets = sumofCompletedTargets + value; 
+               return {label: item.label, value: value, setTarget: item.setTarget, contractType: item.contractType};
+           }))
+           //console.log('я передам такие параметры: sumofTargets:', sumOfTargets,'keyresults:',JSON.stringify(this.keyResults),'ļistOfTargets' ,JSON.stringify(listOfTargets),'для id', this.keyResultId );
+           this.keyResults = await updateKeyResultProgress({sumOfTargets:sumOfTargets, keyResults: this.keyResults, sumofCompletedTargets: sumofCompletedTargets});
         }
     }
+    @track contractTypeOptions;
+    //retrieve all values from picklist 
+    async loadPicklist(){
+        let result = await getContractTypes();
+        //console.log('result from picklists', JSON.stringify(result));
+        this.contractTypeOptions = result.map(picklistValue=>({
+            label: picklistValue, value: picklistValue
+        }));
+        //console.log('contractTypeOptions: ', JSON.stringify(this.contractTypeOptions));
+    }
+
     //handler for opening window with related fields
     handleSetTargetsWindow(event)
     {
@@ -35,21 +104,40 @@ export default class KeyResultList extends LightningElement
     //hadler of Set buuton that will 'subscribe' on fields that need to be tracked to complete Key result
     async handleSetTargetFields(){
         this.showTargetWindow = false; 
+        let value; let targetFromDictionary;
         try {
+            //current related fields
             this.targets = await Promise.all (this.trackedFields.map(async field => {
-                let value = await this.getAmountOfRelatedField(field);
-                return {label: field, value: value};
+                value = await this.getAmountOfRelatedField(field);
+                targetFromDictionary = this.dictionary[field] !==-1 ? this.dictionary[field] : 0;
+                console.log('contract type in handleSetTargetFields',this.contractType,'isundefined?:',this.contractType==null);
+                return {label: field, value: value, setTarget: targetFromDictionary,contractType: this.contractType};
             }));
+            localStorage.setItem(this.keyResultId,JSON.stringify(this.targets));
         } catch (error) {
-            console.log('handleSetTargetFields error: ',JSON.stringify(error));
+            console.log('handleSetTargetFields error: ',error);
         }
     }
-    //handler of checked boxes changes  
+    @track targetedFields=[];
+    @track targetedFieldsWithContracts = [];
+    //handler of checked boxes changes 
     handleSelectedTargetFieldOption(event){
         this.trackedFields = event.detail.value;
-        this.targetedFields = event.detail.value
-    }
-    // returns count of records in objects that relate to particular keyResult record  
+        this.targetedFields = [...this.trackedFields]; // обновляем targetedFields
+    
+        // Удаляем элементы из targetedFieldsWithContracts, если они больше не отмечены
+        this.targetedFieldsWithContracts = this.targetedFieldsWithContracts.filter(field => this.trackedFields.includes(field.value));
+    
+        // Добавляем новые элементы в targetedFieldsWithContracts
+        for(let a of this.trackedFields){
+            if(!this.targetedFieldsWithContracts.some(field => field.value === a)){
+                this.targetedFieldsWithContracts.push({
+                    value: a,
+                    isContract: a === 'Contract'
+                });
+            }
+        }
+    } 
     async getAmountOfRelatedField(field){ 
         if(field=='Opportunity') {
             return Promise.all(this.keyResults.map(async keyResult => {
@@ -120,5 +208,20 @@ export default class KeyResultList extends LightningElement
                     return 0;   
             }));
         }
+        if(field=='Contract'){
+            return Promise.all(this.keyResults.map(async keyResult => {
+                if(keyResult && Array.isArray(keyResult.Contracts__r))
+                    return keyResult.Contracts__r.length;
+                else 
+                    return 0;   
+            }));
+        }
+    }
+    handlerSetTarget(event){
+        this.dictionary[event.target.name] = event.target.value;
+    }
+    handleSelectedTypeContract(event){
+        this.contractType = event.detail.value;
+        console.log('hadle change from picklist, value =:', this.contractType)
     }
 }
